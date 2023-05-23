2ce645d4bb1d02f3962d92217d5395deaea90237
Maajesty <wildanmaulana7891@gmail.com>
2023-05-23 23:39:28 +0800

    project day 2

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function hitungluas() {
-    var sisiluas = document.getElementById("sisi-luas").value;
-
-    document.getElementById("output-luas").innerHTML = sisiluas
-    console.log(sisiluas);
+function hitungluas(){
+    let nilai luas = document.getElementById("sisi-luas").value
+    
+    let result-luas = parseInt(sisi-luas)
+    document.log(sisi-luas)
 
 }
+
+function hitungkeliling(){
+    let nilai keliling = document.getElementById("sisi-keliling").value
     
+    let result-keliling= parseInt(sisi-keliling)
+    document.log(sisi-keliling)
+}
+
